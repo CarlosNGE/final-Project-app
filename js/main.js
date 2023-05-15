d80c5ef2fa9bdbbd027493133eb9c09e3ee007b7
Juan Carlos <jcarlosnge@gmail>
2023-05-15 02:16:17 -0700

    fix grid for random cards

--- js/main.js
+++ js/main.js
@@ -88,7 +88,8 @@ const randomeCard = () =>{
             const cardElement = document.getElementById("yugiohcard");
             cardElement.innerHTML = "";
 
-            
+            const randomContainer =document.getElementById("randomcards");
+            randomContainer.innerHTML = "";
             
             cardElement.appendChild(nameCard);
             cardElement.appendChild(descCard);
@@ -161,6 +162,7 @@ document.getElementById("cardButton").addEventListener("click", randomeCard);
           const myRam3 = Math.round(Math.random() * res.data.length);
           const myRam4 = Math.round(Math.random() * res.data.length);
           const myRam5 = Math.round(Math.random() * res.data.length);
+          const myRam6 = Math.round(Math.random() * res.data.length);
           
 
 
@@ -169,11 +171,16 @@ document.getElementById("cardButton").addEventListener("click", randomeCard);
           const card3 = res.data[myRam3].card_images[0].image_url;
           const card4 = res.data[myRam4].card_images[0].image_url;
           const card5 = res.data[myRam5].card_images[0].image_url;
+          const card6 = res.data[myRam6].card_images[0].image_url;
 
           const cardElement = document.getElementById("yugiohcard");
           cardElement.innerHTML = "";
 
+          const randomContainer =document.getElementById("randomcards");
+          randomContainer.innerHTML = "";
+
           let popup = document.getElementById("popup");
+          
           let closePop = document.getElementById("closepop");
 
           const cardPop = document.getElementById("info");
@@ -360,36 +367,72 @@ document.getElementById("cardButton").addEventListener("click", randomeCard);
             cardPop.appendChild(c5Def);
           }
 
+          function openPopup6() {
+            cardPop.innerHTML = "";
+            popup.classList.add("open-popup");
+
+            let c6Popimg = document.createElement("img");
+            c6Popimg.src = res.data[myRam6].card_images[0].image_url;
+            c6Popimg.className = "cardimg"
+            cardPop.appendChild(c6Popimg);
+
+            const c6Name = document.createElement("h2");
+            c6Name.textContent = res.data[myRam6].name;
+            c6Name.className = "cardtitle";
+            cardPop.appendChild(c6Name);
+            
+            const c6Type = document.createElement("p");
+            c6Type.textContent = res.data[myRam6].type;
+            c6Type.className = "cardtype";
+            cardPop.appendChild(c6Type);
+
+            const c6Desc = document.createElement("p");
+            c6Desc.textContent = res.data[myRam6].desc;
+            c6Desc.className = "carddesc";
+            cardPop.appendChild(c6Desc);
+
+            const c6Atk = document.createElement("p");
+            c6Atk.textContent = res.data[myRam6].atk;
+            c6Atk.className = "atkpower";
+            cardPop.appendChild(c6Atk);
+
+            const c6Def = document.createElement("p");
+            c6Def.textContent = res.data[myRam6].def;
+            c6Def.className = "defpower";
+            cardPop.appendChild(c6Def);
+          }
+
          
-
-          
-
           function closePopup() {
             popup.classList.remove("open-popup");
           }
+          
 
-
-          // Card 1
+          // Cards
 
           let c1 = document.createElement("img");
           c1.src = card1;
-          c1.className = "cardimg";
+          c1.className = "ramcardimg";
 
           let c2 =document.createElement("img");
           c2.src = card2;
-          c2.className = "cardimg2";
+          c2.className = "ramcardimg2";
 
           let c3 =document.createElement("img");
           c3.src = card3;
-          c3.className = "cardimg3";
+          c3.className = "ramcardimg3";
 
           let c4 =document.createElement("img");
           c4.src = card4;
-          c4.className = "cardimg4";
+          c4.className = "ramcardimg4";
 
           let c5 =document.createElement("img");
           c5.src = card5;
-          c5.className = "cardimg5";
+          c5.className = "ramcardimg5";
+
+          let c6 =document.createElement("img");
+          c6.src = card6;
+          c6.className = "ramcardimg6";
 
           
 
@@ -398,193 +441,27 @@ document.getElementById("cardButton").addEventListener("click", randomeCard);
           c3.addEventListener("click", openPopup3);
           c4.addEventListener("click", openPopup4);
           c5.addEventListener("click", openPopup5);
-          
-          
+          c6.addEventListener("click", openPopup6);
         
 
           closePop.addEventListener("click", closePopup);
-
-          
-
-          // const c1Name = document.createElement("h2");
-          // c1Name.textContent = res.data[myRam].name;
-          // c1Name.className = "cardtitle";
-
-          // const c1Type = document.createElement("p");
-          // c1Type.textContent = res.data[myRam].type;
-          // c1Type.className = "cardtype";
-
-          // const c1Desc = document.createElement("p");
-          // c1Desc.textContent = res.data[myRam].desc;
-          // c1Desc.className = "carddesc";
-
-          // const c1Atk = document.createElement("p");
-          // c1Atk.textContent = res.data[myRam].atk;
-          // c1Atk.className = "atkpower";
-
-          // const c1Def = document.createElement("p");
-          // c1Def.textContent = res.data[myRam].def;
-          // c1Def.className = "defpower";
-
-          // //Card 2
-
-          
-
-          // const c2Name = document.createElement("h2");
-          // c2Name.textContent = res.data[myRam2].name;
-          // c2Name.className = "cardtitle2";
-
-          // const c2Type = document.createElement("p");
-          // c2Type.textContent = res.data[myRam2].type;
-          // c2Type.className = "cardtype2";
-
-          // const c2Desc = document.createElement("p");
-          // c2Desc.textContent = res.data[myRam2].desc;
-          // c2Desc.className = "carddesc2";
-
-          // const c2Atk = document.createElement("p");
-          // c2Atk.textContent = res.data[myRam2].atk;
-          // c2Atk.className = "atkpower2";
-
-          // const c2Def = document.createElement("p");
-          // c2Def.textContent = res.data[myRam2].def;
-          // c2Def.className = "defpower2";
-
-          // //Card 3
-
-          
-
-          // const c3Name = document.createElement("h2");
-          // c3Name.textContent = res.data[myRam3].name;
-          // c3Name.className = "cardtitle3";
-
-          // const c3Type = document.createElement("p");
-          // c3Type.textContent = res.data[myRam3].type;
-          // c3Type.className = "cardtype3";
-
-          // const c3Desc = document.createElement("p");
-          // c3Desc.textContent = res.data[myRam3].desc;
-          // c3Desc.className = "carddesc3";
-
-          // const c3Atk = document.createElement("p");
-          // c3Atk.textContent = res.data[myRam3].atk;
-          // c3Atk.className = "atkpower3";
-
-          // const c3Def = document.createElement("p");
-          // c3Def.textContent = res.data[myRam3].def;
-          // c3Def.className = "defpower3";
-
-          // //Card 4
-
-          
-
-          // const c4Name = document.createElement("h2");
-          // c4Name.textContent = res.data[myRam4].name;
-          // c4Name.className = "cardtitle4";
-
-          // const c4Type = document.createElement("p");
-          // c4Type.textContent = res.data[myRam4].type;
-          // c4Type.className = "cardtype4";
-
-          // const c4Desc = document.createElement("p");
-          // c4Desc.textContent = res.data[myRam4].desc;
-          // c4Desc.className = "carddesc4";
-
-          // const c4Atk = document.createElement("p");
-          // c4Atk.textContent = res.data[myRam4].atk;
-          // c4Atk.className = "atkpower4";
-
-          // const c4Def = document.createElement("p");
-          // c4Def.textContent = res.data[myRam4].def;
-          // c4Def.className = "defpower4";
-
-          // //Card 5
-
-          
-
-          // const c5Name = document.createElement("h2");
-          // c5Name.textContent = res.data[myRam5].name;
-          // c5Name.className = "cardtitle5";
-
-          // const c5Type = document.createElement("p");
-          // c5Type.textContent = res.data[myRam5].type;
-          // c5Type.className = "cardtype5";
-
-          // const c5Desc = document.createElement("p");
-          // c5Desc.textContent = res.data[myRam5].desc;
-          // c5Desc.className = "carddesc5";
-
-          // const c5Atk = document.createElement("p");
-          // c5Atk.textContent = res.data[myRam5].atk;
-          // c5Atk.className = "atkpower5";
-
-          // const c5Def = document.createElement("p");
-          // c5Def.textContent = res.data[myRam5].def;
-          // c5Def.className = "defpower5";
-
-         
-
-          
-
-          
-
-          
-          
-
-          
-          
 
           console.log(c1);
           console.log(res.data[myRam].name);
           console.log(res.data[myRam].type);
           console.log(res.data[myRam].desc);
 
-          // console.log(c2);
-          // console.log(c2Name);
+         
          
 
-          cardElement.append(c1);     
-          cardElement.append(c1Name);
-          cardElement.append(c1Type);
-          cardElement.append(c1Desc);
-          cardElement.append(c1Atk);
-          cardElement.append(c1Def);
-
-          cardElement.append(c2);
-          cardElement.append(c2Name);
-          cardElement.append(c2Type);
-          cardElement.append(c2Desc);
-          cardElement.append(c2Atk);
-          cardElement.append(c2Def);
-
-          cardElement.append(c3);
-          cardElement.append(c3Name);
-          cardElement.append(c3Type);
-          cardElement.append(c3Desc);
-          cardElement.append(c3Atk);
-          cardElement.append(c3Def);
-
-          cardElement.append(c4);
-          cardElement.append(c4Name);
-          cardElement.append(c4Type);
-          cardElement.append(c4Desc);
-          cardElement.append(c4Atk);
-          cardElement.append(c4Def);
-
-          cardElement.append(c5);
-          cardElement.append(c5Name);
-          cardElement.append(c5Type);
-          cardElement.append(c5Desc);
-          cardElement.append(c5Atk);
-          cardElement.append(c5Def);
-
-
+          randomContainer.append(c1);     
+          randomContainer.append(c2);
+          randomContainer.append(c3);
+          randomContainer.append(c4);
+          randomContainer.append(c5);
+          randomContainer.append(c6);
           
-
-         
-
-
-         
+          
 
         })
 
@@ -592,7 +469,7 @@ document.getElementById("cardButton").addEventListener("click", randomeCard);
 
       const id = e.target.value;
       
-      // console.log(id);
+      
 
 
      });
